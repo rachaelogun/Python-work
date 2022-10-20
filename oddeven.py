7812f151f7529fbfc9ca95e32b4f67354b4ffb0c
rachaelogun <realso23@yahoo.com>
2022-10-20 15:49:13 +0100

    new work oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -4,7 +4,8 @@ SOMETHING = int(input('Exam check part 3:'))
 count = 0
 while count < 9:
  count = count + 3
- if SOMETHING 
-     print(count, "is even")
+ if SOMETHING < 9:
+     SOMETHING = SOMETHING + 2
+     print(SOMETHING, "is even")
  else:
      print(count, "is odd")
