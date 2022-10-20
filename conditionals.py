91f2a0fedbc69fa8867eaefe445f42cfcfa6a927
rachaelogun <realso23@yahoo.com>
2022-10-20 10:36:15 +0100

    exe2

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,19 @@
-exam1 = int(input('Enter number of people in bus:'))
+from ast import Pass
+
+#longer conditional statement
+exam = int(input('Exam check part 1:'))
+exam1 = 66
+exam2 = 65
+
+if exam1 < exam2:
+    print ("pass")
+else:
+    print("fail")
+
+
+#short ifelse conditional statement
+
+exam1 = int(input('Exam check part 2:'))
 
 if exam1 > 65:
     print("pass")
